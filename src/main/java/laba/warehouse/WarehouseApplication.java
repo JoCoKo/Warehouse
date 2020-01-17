51930b12ca7ac7c770a35c145693c191b6350495
JoCoKo <jocoko@ya.ru>
2020-01-17 22:45:56 +0300

    enabled eureka discovery service support

--- src/main/java/laba/warehouse/WarehouseApplication.java
+++ src/main/java/laba/warehouse/WarehouseApplication.java
@@ -2,8 +2,10 @@ package laba.warehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableDiscoveryClient
 @EnableMongoRepositories
 @SpringBootApplication
 public class WarehouseApplication {
